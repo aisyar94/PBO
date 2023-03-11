490e887155dc506ded8759a846cce8012c4dc529
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-10 20:19:08 -0800

    Create Oper3.java

---
+++ Oper3.java
@@ -0,0 +1,22 @@
+/*NIM   : 13020210004
+Nama    : Aisyah Safira Rachman
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 08:30 PM*/
+package tugas14;
+
+public class Oper3 {
+    
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Algoritma */
+        if (true && true){ System.out.println(true && true); }      /* true = true and true */
+        if (true & true) { System.out.println(true & false); }      /* true & true */
+        if (true) { System.out.println(true); }                     /* true */
+        if (true || true){ System.out.println(true); }              /* true = true or true */
+        if (true|false) { System.out.println(true|false); }         /* true|false */
+        
+    }
+}

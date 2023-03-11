e108e9dfea0fdbb5223bb50c882ecdd18a29f363
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-10 20:01:59 -0800

    Create 4_BacaData.java

---
+++ 4_BacaData.java
@@ -0,0 +1,27 @@
+/*NIM   : 13020210004
+Nama    : Aisyah Safira Rachman
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 07:00 PM*/
+package tugas4;
+
+import java.util.Scanner;
+
+public class BacaData {
+
+    /**
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        //TODO Auto-generated method stub
+        /* Kamus */
+        int a;
+        Scanner masukan;
+        /* Program */
+        System.out.print("Contoh membaca dan menulis, ketik nilai integer : \n");
+        masukan = new Scanner(System.in);
+        a = masukan.nextInt(); /* coba ketik : masukan.nextInt(); Apa akibatnya?*/
+        System.out.print("Nilai yang dibaca : " +a);
+    }
+    
+}

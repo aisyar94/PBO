2b3d9fd26a50b51eb8cc465a1c2292ff1b94f335
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-10 20:15:26 -0800

    Create Incr.java

---
+++ Incr.java
@@ -0,0 +1,25 @@
+/*NIM   : 13020210004
+Nama    : Aisyah Safira Rachman
+Hari/Tgl: Jumat, 10 Maret 2023
+Waktu   : 08:17 PM*/
+package tugas11;
+
+/* Effek dari operator ++ */
+public class Incr {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        int i, j;
+
+        /* Program */
+        i = 3;
+        j = i++;
+
+        System.out.println("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+    
+}
